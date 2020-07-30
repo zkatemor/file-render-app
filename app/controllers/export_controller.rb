5094c26991be1e9b06108117f928bfa276fc5cd4
Ekaterina Zababurina <ea.zababurina@gmail.com>
2020-07-30 12:01:09 +0300

    render xls, xlsx, html & pdf files

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -1,21 +1,15 @@
 class ExportController < ApplicationController
   def show
-    # html = params[:html]
-
-    ac = ActionController::Base.new()
-    html_string = ac.render_to_string :template => 'export/pdfkit/response.xml'
-
     respond_to do |format|
       format.html
       format.pdf do
+        html_string = ActionController::Base.new().render_to_string :template => 'export/show.html.erb'
+
         kit = PDFKit.new(html_string, :page_size => 'A4')
+
         send_data(kit.to_pdf, :filename => "pdfkit.pdf")
       end
-      format.xls do
-        send_data html_string,
-                  :type => 'application/vnd.ms-excel; charset=utf-8; header=present',
-                  :filename => 'export.xls'
-      end
+      format.xls
       format.xlsx
     end
   end
