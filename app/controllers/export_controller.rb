007941115bb2ce52c376e6d63cf5f56853da83ed
Ekaterina Zababurina <ea.zababurina@gmail.com>
2020-07-31 16:57:47 +0300

    fix xlsx render

--- app/controllers/export_controller.rb
+++ app/controllers/export_controller.rb
@@ -9,8 +9,16 @@ class ExportController < ApplicationController
 
         send_data(kit.to_pdf, :filename => "pdfkit.pdf")
       end
-      format.xls
-      format.xlsx
+      format.xls do
+        render template: 'export/show.xls.erb'
+      end
+      format.xlsx do
+        @data = Array.new
+        @data << %w[1 Test1 Test1 test1@test.com 666-666-666 www.test.uk 1 online]
+        @data << %w[2 Test2 Test2 test2@test.com 666-666-666 www.test.ru 2 offline]
+        @data << %w[3 Test3 Test3 test3@test.com 666-666-666 www.test.uk 3 online]
+        render template: 'export/show.xlsx.axlsx'
+      end
     end
   end
 end
